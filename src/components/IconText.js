e92ee568024ede42e19297d2fbb4a47bda640453
Lucashylario <lucashyfrotend@hotmail.com>
2021-07-26 10:01:16 -0300

    adicionado "mini if else"

--- src/components/IconText.js
+++ src/components/IconText.js
@@ -3,16 +3,16 @@ import { Col, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-const IconText = ({ icon, size = '1x', color = '#000', className = '', children, }) => (
+const IconText = ({ icon, size = 1, color = '#000', className = '', children, }) => (
     <Row>
-        <Col xs={2} className='d-flex justify-content-center'>
+        <Col xs={typeof size == Number ? `${size}x` : size} className='d-flex justify-content-center'>
             <FontAwesomeIcon 
                 icon={icon}
                 size={size}
                 color={color}
             />
         </Col>
-        <Col xs={10} className={className}>
+        <Col xs={12 - size} className={className}>
             { children }
         </Col>
     </Row>
