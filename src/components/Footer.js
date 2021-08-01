74cb455c1d31c94c86ca493fc544212aa5b8249f
Lucashylario <lucashyfrotend@hotmail.com>
2021-08-01 10:06:31 -0300

    add style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faYoutube, } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookSquare, faTwitter, faYoutube, } from '@fortawesome/free-brands-svg-icons';
 import logo from '../assets/images/footer-logo.png';
 import appStore from '../assets/images/appStore.png';
 import googlePlay from '../assets/images/googlePlay.png';
+import './Footer.scss';
 
 const Footer = () => (
-    <footer>
+    <footer className='footer'>
         <Container>
-            <Row>
-                <Col xs={12} lg={6}>
+            <Row className='text-center py-5'>
+                <Col xs={12} lg={6} className='text-lg-left'>
                     <Image src={logo} />
                 </Col>
                 <Col xs={12} lg={4}>
                     <Image src={appStore} />
-                    <Image src={googlePlay} />
+                    <Image src={googlePlay} className='img-space' />
                 </Col>
-                <Col xs={12} lg={2}>
-                    <FontAwesomeIcon icon={faFacebook} />
-                    <FontAwesomeIcon icon={faTwitter} />
-                    <FontAwesomeIcon icon={faYoutube} />
+                <Col xs={12} lg={2} className='d-flex align-items-center justify-content-center'>
+                    <a href='http://facebook.com/' target='_blank' rel="noreferrer">
+                    
+                        <FontAwesomeIcon icon={faFacebookSquare} color='#7c7c7c' size='2x' />
+                    </a> 
+                    <a href='http://twitter.com/' target='_blank' rel="noreferrer">
+                        <FontAwesomeIcon icon={faTwitter} color='#7c7c7c' size='2x' className='img-space' />
+                    </a>
+                    <a href='http://youtube.com/' target='_blank' rel="noreferrer">
+                        <FontAwesomeIcon icon={faYoutube} color='#7c7c7c' size='2x' className='img-space' />
+                    </a>
                 </Col>
             </Row>
         </Container>
